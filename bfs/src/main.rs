82d37854a24e0a19022ebcfe4134dc716c7d449d
Brian Pattie <bpattie@uvic.ca>
2018-10-14 08:39:12 -0800

    fixed bfs, added new solution bfs2

--- bfs/src/main.rs
+++ bfs/src/main.rs
@@ -1,15 +1,15 @@
-extern crate std_semaphore;
+// extern crate std_semaphore;
 extern crate queues;
 
 use std::thread;
 use std::sync::{RwLock, Mutex, Arc};
-use std_semaphore::Semaphore;
-use std::time::Duration;
+// use std_semaphore::Semaphore;
+// use std::time::Duration;
 use queues::*;
 
-const START: i32 = 1; // Start Node
-const TARGET: i32 = 4; // End Node
-const NODES: i32 = 7; // Number of Nodes
+const START: usize = 1; // Start Node
+const TARGET: usize = 4; // End Node
+const NODES: usize = 7; // Number of Nodes
 
 fn main() {
 
@@ -22,62 +22,68 @@ fn main() {
             [1, 0, 0, 0, 0, 0, 1,],
             [0, 0, 0, 1, 1, 1, 0,],
             ];
-    let frontier = Arc::new(Mutex::new(Queue::<i32>::new()));
-    let visited = Arc::new(RwLock::new(Vec::new())); // Vector of size NODES
-    let path = Arc::new(Mutex::new(Vec::new())); // Vector of size NODES
+    let frontier = Arc::new(Mutex::new(Queue::<usize>::new()));
+    let mut f = frontier.lock().unwrap();
+    f.add(START);
+    drop(f);
 
-    //Initialize visited/path
-    let x = visited.clone();
-    x = vec![0; NODES];
-    drop(x);
-    let y = path.clone();
-    y = vec![0; NODES];
-    drop(y);
+    let visited = Arc::new(RwLock::new(vec![false; NODES])); // Vector of size NODES
+    let path = Arc::new(Mutex::new(vec![0; NODES])); // Vector of size NODES
+    let success = Arc::new(Mutex::new(false));
 
     let mut handles;
+
+    // println!("beginning loop");
 
     loop {
 
         handles = vec![];
 
-        let front_main = frontier.clone(); // FIXME Try skipping the clone and just using frontier directly
-        let mut f = front_main.lock().unwrap();
-        while f.size() != 0 {
+        // println!("beginning loop");
+        let mut f = frontier.lock().unwrap();
+
+        while f.size() != 0 { // For every node in the frontier
             let s = f.remove().unwrap();
-            for i in adj[s] {
+            for i in 0..NODES { // Iterate over each node's edges
                 if adj[s][i] != 0 {
-                    //LOOP BEGIN
+
                     let front_c = frontier.clone();
                     let vis_c = visited.clone();
                     let path_c = path.clone();
+                    let success_c = success.clone();
 
-                    let handle = thread::spawn(move || {
+                    let handle = thread::spawn(move || { // Create a thread to traverse the edge
+
+                        // println!("beginning thread s={} d={}", s, i);
 
                         let source = s; // TODO SET VALUES FOR SOURCE AND DEST, SCRAP x,y
                         let dest = i; // TODO SET VALUES FOR SOURCE AND DEST, SCRAP x,y
 
-                        let mut v = vis.read().unwrap();
+                        let mut v = vis_c.read().unwrap();
                         if v[dest] == false {
                             drop(v);
-                            let mut v = vis.write().unwrap();
+                            let mut v = vis_c.write().unwrap();
                             v[dest] = true;
                             drop(v);
 
                             let mut p = path_c.lock().unwrap();
-                            path[dest] = source;
+                            p[dest] = source;
                             drop(p);
 
-                            if dest == TARGET {
+                            if dest == TARGET { // SUCCESS
 
                                 let mut p = path_c.lock().unwrap();
                                 // This prints the path in reverse order
-                                let trace = dest;
-                                loop{
+                                let mut trace = dest;
+                                loop {
                                     println!("{}", trace);
-                                    trace = path[trace];
+                                    trace = p[trace];
                                     if trace == START { break };
                                 }
-                                // drop(p) // Unnecessary
+                                println!("{}", trace);
+                                let mut suc = success_c.lock().unwrap();
+                                *suc = true;
+                                drop(suc);
                             } else {
                                 let mut f = front_c.lock().unwrap();
                                 f.add(dest);
@@ -92,5 +98,11 @@ fn main() {
         for handle in handles {
             handle.join().unwrap();
         }
+        let mut suc = success.lock().unwrap();
+        if *suc == true {
+            break;
+        }
+        drop(suc);
     }   // End of BFS Loop
+    println!("Solution Found");
 }
---
+++ bfs2/src/main.rs
@@ -0,0 +1,109 @@
+extern crate std_semaphore;
+extern crate queues;
+
+use std::thread;
+use std::sync::{RwLock, Mutex, Arc};
+use std_semaphore::Semaphore;
+// use std::time::Duration;
+use queues::*;
+
+const START: usize = 0; // Start Node
+const TARGET: usize = 1; // TARGET Node
+const NODES: usize = 7; // Number of Nodes
+const THREADS: usize = 2; // Number of Threads to use
+
+fn main() {
+
+    let adj = Arc::new(RwLock::new([
+            [0, 1, 0, 1, 0, 1, 0,],
+            [1, 0, 1, 0, 0, 0, 0,],
+            [0, 1, 0, 1, 0, 0, 0,],
+            [1, 0, 1, 0, 0, 0, 1,],
+            [0, 0, 0, 0, 0, 0, 1,],
+            [1, 0, 0, 0, 0, 0, 1,],
+            [0, 0, 0, 1, 1, 1, 0,],
+            ]));
+    let frontier = Arc::new(Mutex::new(Queue::<usize>::new()));
+    let f_items  = Arc::new(Semaphore::new(0)); // Stops threads from accessing an empty Queue
+    let success  = Arc::new(Semaphore::new(0));
+    let visited  = Arc::new(RwLock::new(vec![false; NODES])); // Vector of size NODES TODO INITIALIZE
+    let path     = Arc::new(Mutex::new(vec![0; NODES])); // Vector of size NODES TODO INITIALIZE
+
+    let mut f = frontier.lock().unwrap();
+    f.add(START);
+    f_items.release();
+    drop(f);
+
+    // Begin thread creation loop
+    for _i in 0..THREADS {
+
+        // Create Clones
+        let a   = adj.clone();
+        let f   = frontier.clone();
+        let f_i = f_items.clone();
+        let v   = visited.clone();
+        let p   = path.clone();
+        let s   = success.clone();
+
+        //let handle =
+        thread::spawn(move || {
+
+            let mut source:usize;
+
+            loop {
+                // Pop next node from frontier
+                f_i.acquire();
+                let mut f_l = f.lock().unwrap();
+                source = f_l.remove().unwrap();
+                drop(f_l);
+
+                let a_r = a.read().unwrap(); // Keep read open, no need to drop it
+                for i in 0..NODES { // For all nodes
+                    if a_r[source][i] > 0 { // If there exists an edge between them
+                        let v_r = v.read().unwrap();
+                        if v_r[i] == false { // Check if it's been visited
+                            // If it has not been visited, mark as visited and process it
+                            drop(v_r); // (Drop read so that write can proceed)
+                            let mut v_w = v.write().unwrap();
+                            v_w[i] = true;
+                            drop(v_w);
+                            if i == TARGET { // If it is the TARGET node, then update the path and print the solution
+                                let mut p_l = p.lock().unwrap();
+                                p_l[i] = source;
+
+                                // TODO PRINT SOLUTION
+                                let mut trace = i;
+                                loop {
+                                    println!("{}", trace);
+                                    trace = p_l[trace];
+                                    if trace == START {
+                                        println!("{}", trace);
+                                        break;
+                                    };
+                                }
+                                // TODO Signal to main thread that a solution has been found
+                                s.release();
+
+                                drop(p_l);
+                            } else { // If it is NOT the TARGET node, then just update the path and add it to the frontier
+                                let mut p_l = p.lock().unwrap();
+                                p_l[i] = source;
+                                drop(p_l);
+                                let mut f_l = f.lock().unwrap();
+                                f_l.add(i);
+                                drop(f_l);
+                                f_i.release();
+                            }
+                        } else { // Else just unlock Visited
+                            drop(v_r);
+                        }
+                    } // No Else or following instructions
+                } // No follwing instructions
+            } // No following instructions
+        });
+    }
+
+    success.acquire();
+    println!("Solution Found");
+
+}
